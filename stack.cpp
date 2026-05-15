cb03e5039249f0f0ddfb233f42761615ad758956
Martzz-a <maritzafidelyah@gmail.com>
2026-05-15 15:00:14 +0700

    Menambahkan Prosedur Peek

--- stack.cpp
+++ stack.cpp
@@ -47,4 +47,22 @@ public:
         top = temp->next;
         delete temp;
     }
+
+    void peek()
+    {
+        if (isEmpty())
+        {
+            cout << "List is empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
 };
