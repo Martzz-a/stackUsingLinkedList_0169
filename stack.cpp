c906bc156877c2689fce4913133dccc57b511d72
Martzz-a <maritzafidelyah@gmail.com>
2026-05-15 15:08:00 +0700

    Menambahkan While Loop untuk Menampilkan Pilihan dan Menjalankan Program

--- stack.cpp
+++ stack.cpp
@@ -78,4 +78,36 @@ int main()
 
     int choice = 0;
     int value;
+
+    while (choice != 5)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+        case 2:
+            stack.pop();
+            break;
+        case 3:
+            stack.peek();
+            break;
+        case 4:
+            cout << "Existing program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
+    }
+    
 }
