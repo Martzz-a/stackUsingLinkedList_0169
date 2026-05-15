2217f6f43cb357bab055bcf085a721c947912cfd
Martzz-a <maritzafidelyah@gmail.com>
2026-05-15 14:44:12 +0700

    Menambahkan Statement Agar fFeld menjadi Top

--- stack.cpp
+++ stack.cpp
@@ -28,6 +28,7 @@ public:
     {
         Node *newNode = new Node(); // 1. Allocate memory for the new node
         newNode->data = value;      // 2. Assign value
+        newNode->next = top;        // 3. Set the next pointer of the new node to the top
         
     }
 };
